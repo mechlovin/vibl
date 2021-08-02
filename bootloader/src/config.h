7def1b2a8b446447cfffce439e9540afbfa16db8
mechlovin <57231893+mechlovin@users.noreply.github.com>
2021-08-02 11:12:31 +0700

    add adelais

--- bootloader/src/config.h
+++ bootloader/src/config.h
@@ -20,6 +20,20 @@
 #define BL_OUTPUT_PIN 12
 #define BL_INPUT_BANK GPIOB
 #define BL_INPUT_PIN 13
+#elif defined(TARGET_WEAVEN)
+#define VIAL_KEYBOARD_UID {0x68, 0xBF, 0xB0, 0x60, 0x95, 0xBB, 0x5C, 0x33}
+/* setup for bluepill */
+#define BL_OUTPUT_BANK GPIOA
+#define BL_OUTPUT_PIN 4
+#define BL_INPUT_BANK GPIOB
+#define BL_INPUT_PIN 12
+#elif defined(TARGET_ADELAIS_F103)
+#define VIAL_KEYBOARD_UID {0x68, 0xBF, 0xB0, 0x60, 0x95, 0xBB, 0x5C, 0x33}
+/* setup for bluepill */
+#define BL_OUTPUT_BANK GPIOB
+#define BL_OUTPUT_PIN 1
+#define BL_INPUT_BANK GPIOA
+#define BL_INPUT_PIN 10
 #else
 #error Unknown target
 #endif
